dd70a814d45d3d13a2c3a6fd3afb8b178e41fe54
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-10-26 15:59:53 +0300

    chore

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 export const Navigation = ({ tabs }) => {
-    return (<nav>
+    return (<nav style={{display: "flex", gap: "10px"}}>
         {tabs.map((tab) => (
           <Link key={tab.id} to={`/${tab.id}`}>
             {tab.title}
